f5762bf48ee584f8743e4a4b3e8c79c82726abb0
Anthony Volk <anth.volk@gmail.com>
2023-04-04 14:41:55 -0500

    feat: Improvements to Signup view

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,19 +1,76 @@
+// External imports
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Signup() {
+
+	// State variable object for controlled input
+	const formObject = {
+		fname: '',
+		lname: '',
+		email: '',
+		password: '',
+		confirmPassword: ''
+	};
+	const validationErrorObject = {
+		fname: '',
+		lname: '',
+		email: '',
+		password: '',
+		confirmPassword: ''
+	};
+
+	const [form, setForm] = useState(formObject);
+
+	// Form input handler
+	function handleFormChange(event) {
+		const { name, value } = event.target;
+		setForm(prev => ({
+			...prev,
+			[name]: value
+		}));
+	};
+
+	// Form submission handler
+	function handleFormSubmit(event) {
+		event.preventDefault();
+
+		// Store form data in new variable;
+		// note that data is accessible via FIELD_NAME.value
+		const formElements = event.target.elements;
+
+
+		// Validate that no fields are empty
+
+
+		// Validate password
+
+
+
+	}
+
+
+	// Logic for confirming password
 
 	return(
 		<div className='Signup'>
 			<div className='Signup_container'>
 				<h1 className='Signup_logoText'>TEXT PLACEHOLDER</h1>
 				<h2 className='Signup_title'>Sign Up</h2>
-				<form action='/signup' method='post' className='Signup_form'>
-					<label for='fname'>First Name</label>
-					<input className='Signup_textInput' type='text' name='fname' placeholder='First Name'></input>
-					<label for='lname'>Last Name</label>
-					<input className='Signup_textInput' type='text' name='lname' placeholder='Last Name'></input>
-					<label for='email'>Email Address</label>
-					<input className='Signup_textInput' type='email' name='email' placeholder='Email Address'></input>
+				<form className='Signup_form' onSubmit={handleFormSubmit}>
+					<label htmlFor='fname'>First Name</label>
+					<input className='Signup_textInput' type='text' name='fname' value={form.fname} onChange={handleFormChange} placeholder='First Name'></input>
+					<label htmlFor='lname'>Last Name</label>
+					<input className='Signup_textInput' type='text' name='lname' value={form.lname} onChange={handleFormChange} placeholder='Last Name'></input>
+					<label htmlFor='email'>Email Address</label>
+					<input className='Signup_textInput' type='email' name='email' value={form.email} onChange={handleFormChange} placeholder='Email Address'></input>
+					<label htmlFor='password'>Password</label>
+					<input className='Signup_textInput' type='password' name='password' value={form.password} onChange={handleFormChange}></input>
+					<label htmlFor='confirmPassword'>Confirm Password</label>
+					<input className='Signup_textInput' type='password' name='confirmPassword' value={form.confirmPassword} onChange={handleFormChange}></input>
+					<button type='submit' className='Util_button'>Sign Up</button>
 				</form>
+				<Link to='/login'>I already have an account</Link>
 			</div>
 		</div>
 	)
