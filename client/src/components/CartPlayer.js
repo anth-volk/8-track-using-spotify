8de7271b383da32195d924b106613b7a5ff4339e
Anthony Volk <anth.volk@gmail.com>
2023-04-28 20:44:29 -0500

    feat: Spotify playback, partly complete

--- client/src/components/CartPlayer.js
+++ client/src/components/CartPlayer.js
@@ -1,8 +1,5 @@
 // External imports
-import { useEffect, useRef, useState } from 'react';
-
-// Internal imports
-import WebPlayback from './WebPlayback.js';
+import { Fragment, useEffect, useRef, useState } from 'react';
 
 export default function CartPlayer(props) {
 
@@ -39,6 +36,7 @@ export default function CartPlayer(props) {
 		activeTracksArray[i] = 	{
 			audio: effects.FADE_IN,
 			type: EFFECT,
+			length: FADE_IN_LENGTH_MS,
 			end_timestamp: FADE_IN_LENGTH_MS,
 			next_spotify_track_index: 0,
 			intra_track_fade_length_ms: activeCart ? parseInt(activeCart[programNumber].intra_track_fade_length_ms) : null
@@ -46,19 +44,162 @@ export default function CartPlayer(props) {
 	}
 
 	const [isCartPlaying, setIsCartPlaying] = useState(false);
-	const [activeProgram, setActiveProgram] = useState(1);
+	// Note that activeProgram will select 0-3; when rendered, if the actual
+	// number is needed, it is imperative to add 1
+	const [activeProgram, setActiveProgram] = useState(0);
+	const [playingAudio, setPlayingAudio] = useState(null);
+
+	const [isPaused, setIsPaused] = useState(false);
+	const [isPlaybackActive, setIsPlaybackActive] = useState(false);
+	const [currentTrack, setCurrentTrack] = useState(null);
+	const [playbackMessage, setPlaybackMessage] = useState('');
 
 	const activeTime = useRef(0);
 	const intervalRef = useRef(null);
 	const activeTracks = useRef(activeTracksArray);
 
-	function handleCartridgePlay() {
-		setIsCartPlaying(true);
+	const spotifyPlayer = useRef(null);
+	const deviceId = useRef(null);
+
+	function handlePlayPause() {
+		if (activeCart) {
+			setIsCartPlaying(prev => !prev);
+		}
 	}
 
-	function handleCartridgePause() {
-		setIsCartPlaying(false);
+	async function startSpotifyPlayback(uri, startTime) {
+		try {
+			const responseRaw = await fetch('https://api.spotify.com/v1/me/player/play', {
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json',
+					'Authorization': 'Bearer ' + spotifyUserAuthToken
+				},
+				body: JSON.stringify({
+					uris: [
+						"spotify:track:" + uri
+					],
+					position_ms: startTime
+				})
+			});
+			if (!responseRaw.ok) {
+				const responseJSON = await responseRaw.json();
+				console.error('Network-related error while initiating Spotify playback: ', responseJSON);
+			}
+		}
+		catch (err) {
+			console.error('Error while initiating Spotify playback: ', err);
+		}
 	}
+
+	// Spotify SDK hook
+	useEffect(() => {
+
+		const script = document.createElement("script");
+		script.src = "https://sdk.scdn.co/spotify-player.js";
+		script.async = true;
+		
+		document.body.appendChild(script);
+		
+		window.onSpotifyWebPlaybackSDKReady = () => {
+		
+			const playerConstructor = new window.Spotify.Player({
+				name: 'Web Playback SDK',
+				getOAuthToken: cb => { cb(spotifyUserAuthToken); },
+				volume: 0.5
+			});
+	
+			spotifyPlayer.current = playerConstructor;
+			// setPlayer(player);
+	
+			spotifyPlayer.current.addListener('ready', ({ device_id }) => {
+				console.log('Ready with Device ID', device_id);
+				deviceId.current = device_id;
+				// setDeviceId(device_id);
+			});
+		
+			spotifyPlayer.current.addListener('not_ready', ({ device_id }) => {
+				console.log('Device ID has gone offline', device_id);
+			});
+		
+			spotifyPlayer.current.addListener('player_state_changed', ( state => {
+	
+				if (!state) {
+					return;
+				}
+	
+				setCurrentTrack(state.track_window.current_track);
+				setIsPaused(state.paused);
+	
+				spotifyPlayer.current.getCurrentState().then( state => { 
+					(!state)? setIsPlaybackActive(false) : setIsPlaybackActive(true) 
+				});
+	
+			}));
+	
+			spotifyPlayer.current.on('playback_error', ({message}) => {
+				console.error('Failed to perform playback', message);
+			})
+	
+			spotifyPlayer.current.connect();
+		
+		};
+	
+	}, []);
+
+	useEffect(() => {
+
+		async function transferPlayback() {
+
+			setPlaybackMessage('Loading Spotify player...');
+
+			try {
+				const fetchResponseRaw = await fetch('https://api.spotify.com/v1/me/player', {
+					method: 'PUT',
+					headers: {
+						'Content-Type': 'application/json',
+						'Authorization': 'Bearer ' + spotifyUserAuthToken
+					},
+					body: JSON.stringify({
+						device_ids: [
+							deviceId.current
+						],
+						play: false
+					})
+				});
+				if (!fetchResponseRaw.ok) {
+					const fetchResponseJSON = await fetchResponseRaw.json();
+					if (fetchResponseJSON && fetchResponseJSON.error && fetchResponseJSON.error.status) {
+						console.log(fetchResponseJSON);
+						switch(fetchResponseJSON.error.status) {
+							case 502:
+								setPlaybackMessage('Error while loading Spotify player; please try again later');
+								break;
+							case 429:
+								setPlaybackMessage('App has exceeded Spotify API limits; please try again in 30+ seconds');
+								break;
+							default:
+								setPlaybackMessage('');
+								break;
+						}
+					}
+				}
+				else {
+					setPlaybackMessage('');
+				}
+			}
+			catch (err) {
+				console.error('Error while transferring Spotify playback: ', err);
+				setPlaybackMessage('Error while loading Spotify player; please try again later');
+			}
+
+		}
+
+		if (deviceId.current && activeCart && !isPlaybackActive) {
+			transferPlayback();
+		}
+
+	}, [deviceId, activeCart, isPlaybackActive]);
 
 	useEffect(() => {
 
@@ -67,6 +208,7 @@ export default function CartPlayer(props) {
 
 		if (isCartPlaying) {
 
+			// Add intra track fade lengths to tracks object
 			activeTracks.current = activeTracks.current.map( (program, index) => {
 				const programNumber = 'program'.concat(index + 1);
 				if (!program.intra_track_fade_length_ms) {
@@ -79,15 +221,17 @@ export default function CartPlayer(props) {
 					return program;
 				}
 			});
+			setPlayingAudio(activeTracks.current[activeProgram]);
 
+			//Every second, map over everything (at end of setInterval)
 			intervalRef.current = setInterval(() => {
-				//Every second, map over everything (at end of setInterval)
 
 				// If activeTime.current reached program length
 				// (including program selector sound), reset all
 				if (activeTime.current === LAST_TRACK_END_TIMESTAMP_MS + FADE_OUT_LENGTH_MS + PROGRAM_SELECTOR_LENGTH_MS) {
 					activeTime.current = 0;
 					activeTracks.current = activeTracksArray;
+					setPlayingAudio(activeTracks.current[activeProgram]);
 					
 				}
 				// If we've reached the end of the fade-out, set all
@@ -98,9 +242,11 @@ export default function CartPlayer(props) {
 							...program,
 							audio: effects.PROGRAM_SELECTOR,
 							type: EFFECT,
+							length: PROGRAM_SELECTOR_LENGTH_MS,
 							end_timestamp: program.end_timestamp + PROGRAM_SELECTOR_LENGTH_MS
 						});
-					})
+					});
+					setPlayingAudio(activeTracks.current[activeProgram]);
 				}
 				// If we've reached the last-track timestamp, set all 
 				// to fade-out audio
@@ -110,24 +256,33 @@ export default function CartPlayer(props) {
 							...program,
 							audio: effects.FADE_OUT,
 							type: EFFECT,
+							length: FADE_OUT_LENGTH_MS,
 							end_timestamp: program.end_timestamp + FADE_OUT_LENGTH_MS
 						});
-					})
+					});
+					setPlayingAudio(activeTracks.current[activeProgram]);
+
 				}
 				// Otherwise...
 				else {
+
+					let currentProgramChanged = false;
 
 					// Map over all tracks
 					activeTracks.current = activeTracks.current.map ( (program, index) => {
 						const programNumber = 'program'.concat(index + 1);
 						// If activeTime.current === program.end_timestamp for any...
 						if (activeTime.current === program.end_timestamp) {
+							if (index === activeProgram) {
+								currentProgramChanged = true;
+							}
 							// If that track is of type Spotify song and there's an inter-track fade length, switch to that
 							if (program.type === SPOTIFY_TRACK && program.intra_track_fade_length_ms > 0) {
 								return ({
 									...program,
 									audio: effects.INTRA_TRACK_FADE,
 									type: EFFECT,
+									length: program.intra_track_fade_length_ms,
 									end_timestamp: program.end_timestamp + program.intra_track_fade_length_ms
 								})
 							}
@@ -139,6 +294,7 @@ export default function CartPlayer(props) {
 									...program,
 									audio: activeCart[programNumber].tracks[program.next_spotify_track_index].spotify_track_id,
 									type: SPOTIFY_TRACK,
+									length: parseInt(activeCart[programNumber].tracks[program.next_spotify_track_index].duration_ms),
 									end_timestamp: program.end_timestamp + parseInt(activeCart[programNumber].tracks[program.next_spotify_track_index].duration_ms),
 									next_spotify_track_index: program.next_spotify_track_index === activeCart[programNumber].tracks.length ? 0 : program.next_spotify_track_index + 1
 								});
@@ -151,14 +307,50 @@ export default function CartPlayer(props) {
 							})
 						}
 					})
+
+					if (currentProgramChanged) {
+						setPlayingAudio(activeTracks.current[activeProgram]);
+					}
 				}
 
+				console.log('activeTime: ', activeTime.current);
+				console.log('activeTracks.current: ', activeTracks.current);
 				activeTime.current = activeTime.current + 1;
+
 			}, 1);
 
 		}
 
 	}, [isCartPlaying, activeProgram]);
+
+	useEffect(() => {
+		setPlayingAudio(activeTracks.current[activeProgram]);
+	}, [activeProgram]);
+
+	useEffect(() => {
+
+		console.log('Playing audio re-rendered');
+		console.log(playingAudio);
+
+		if (isCartPlaying && playingAudio) {
+			// Calculate start timestamp
+			const startingTime = activeTime.current - (playingAudio.end_timestamp - playingAudio.length);
+
+			if (playingAudio.type === EFFECT) {
+				// Play audio file of relevant effect audio
+				return;
+			}
+			else {
+
+				// Load song on Spotify
+				startSpotifyPlayback(playingAudio.audio, startingTime);
+
+				// Play song
+
+			}
+		}
+
+	}, [playingAudio, isCartPlaying]);
 
 	// Clear any existing timeouts upon re-render
 	useEffect(() => {
@@ -166,7 +358,19 @@ export default function CartPlayer(props) {
 	}, []);
 
 	return (
-		<WebPlayback authToken={spotifyUserAuthToken}/>
+		<Fragment>
+			<div className='container'>
+				<div className='main-wrapper'>
+					{/*Empty 8-track player visual*/}
+					{/*Inside of that: activeCart details, if present*/}
+					{ activeCart && <p className='activeCart_details'>{activeCart.cart_name}</p>}
+					<p className='playbackMessage'>{playbackMessage}</p>	
+				</div>
+				<button type='button' className={`playbackButton ${activeCart && isPlaybackActive && !playbackMessage ? 'active' : 'disabled'}`} onClick={handlePlayPause}>
+					{ isCartPlaying ? 'PAUSE' : 'PLAY'}
+				</button>
+			</div>
+		</Fragment>
 	)
 
 }
--- client/src/components/WebPlayback.js
+++ client/src/components/WebPlayback.js
@@ -1,142 +1,122 @@
-import React, { useEffect, useState } from 'react';
-
-export default function WebPlayback(props) {
-
-	const spotifyUserAuthToken = props.authToken || null;
-
-	const track = {
-		name: "",
-		album: {
-			images: [
-				{ url: "" }
-			]
-		},
-		artists: [
-			{ name: "" }
-		]
-	}
-
-	const [player, setPlayer] = useState(null);
-	const [isPaused, setIsPaused] = useState(false);
-	const [isPlaybackActive, setIsPlaybackActive] = useState(false);
-	const [currentTrack, setCurrentTrack] = useState(track);
-	const [deviceId, setDeviceId] = useState(null);
-
-	// Spotify SDK hook
-	useEffect(() => {
-
-		const script = document.createElement("script");
-		script.src = "https://sdk.scdn.co/spotify-player.js";
-		script.async = true;
-	
-		document.body.appendChild(script);
-	
-		window.onSpotifyWebPlaybackSDKReady = () => {
-	
-			const player = new window.Spotify.Player({
-				name: 'Web Playback SDK',
-				getOAuthToken: cb => { cb(spotifyUserAuthToken); },
-				volume: 0.5
-			});
-
-			setPlayer(player);
-
-			player.addListener('ready', ({ device_id }) => {
-				console.log('Ready with Device ID', device_id);
-				setDeviceId(device_id);
-			});
-	
-			player.addListener('not_ready', ({ device_id }) => {
-				console.log('Device ID has gone offline', device_id);
-			});
-	
-			player.addListener('player_state_changed', ( state => {
-
-                if (!state) {
-                    return;
-                }
-
-                setCurrentTrack(state.track_window.current_track);
-                setIsPaused(state.paused);
-
-                player.getCurrentState().then( state => { 
-                    (!state)? setIsPlaybackActive(false) : setIsPlaybackActive(true) 
-                });
-
-            }));
-
-			player.on('playback_error', ({message}) => {
-				console.error('Failed to perform playback', message);
-			})
-
-			player.connect();
-	
-		};
-
-	}, []);
-
-	useEffect(() => {
-
-		async function transferPlayback() {
-
-			await fetch('https://api.spotify.com/v1/me/player', {
-				method: 'PUT',
-				headers: {
-					'Content-Type': 'application/json',
-					'Authorization': 'Bearer ' + spotifyUserAuthToken
-				},
-				body: JSON.stringify({
-					device_ids: [
-						deviceId
-					],
-					play: true
-				})
-			});
-
-		}
-
-		if (deviceId) {
-			transferPlayback();
-		}
-
-	}, [deviceId])
-
-	if (!isPlaybackActive) { 
-        return (
-            <>
-                <div className="container">
-                    <div className="main-wrapper">
-                        <b> Instance not active. Transfer your playback using your Spotify app </b>
-                    </div>
-                </div>
-            </>)
-    } else {
-        return (
-            <>
-                <div className="container">
-                    <div className="main-wrapper">
-
-                        <img src={currentTrack.album.images[0].url} className="now-playing__cover" alt="" />
-
-                        <div className="now-playing__side">
-                            <div className="now-playing__name">{currentTrack.name}</div>
-                            <div className="now-playing__artist">{currentTrack.artists[0].name}</div>
-
-                            <button className="btn-spotify" onClick={() => { player.previousTrack() }} >
-                                &lt;&lt;
-                            </button>
-
-                            <button className="btn-spotify" onClick={() => { player.togglePlay() }} >
-                                { isPaused ? "PLAY" : "PAUSE" }
-                            </button>
-
-                            <button className="btn-spotify" onClick={() => { player.nextTrack() }} >
-                                &gt;&gt;
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </>
-        );
-    }
-}
+// import React, { Fragment, useEffect, useRef, useState } from 'react';
+// 
+// export default function WebPlayback(props) {
+// 
+// 	/*
+// 	const activeCart = props.activeCart || null;
+// 	const spotifyUserAuthToken = props.authToken || null;
+// 	const activePlayback = props.activePlayback || null;
+// 	const activeTime = props.activeTime || null;
+// 
+// 	console.log(activeCart);
+// 	console.log('activePlayback: ', activePlayback);
+// 	*/
+// 
+// 	// const [player, setPlayer] = useState(null);
+// 	const [isPaused, setIsPaused] = useState(false);
+// 	const [isPlaybackActive, setIsPlaybackActive] = useState(false);
+// 	const [currentTrack, setCurrentTrack] = useState(track);
+// 	// const [deviceId, setDeviceId] = useState(null);
+// 
+// 	const player = useRef(null);
+// 	const deviceId = useRef(null);
+// 
+// 	const playerJSX = activeCart && isPlaybackActive
+// 		? <p className='activeCart_details'>{activeCart.cart_name}</p>
+// 		: activeCart
+// 			? <p className='activeCart_message'>Loading player, please wait...</p>
+// 			: null;
+// 
+// 	// Spotify SDK hook
+// 	useEffect(() => {
+// 
+// 		const script = document.createElement("script");
+// 		script.src = "https://sdk.scdn.co/spotify-player.js";
+// 		script.async = true;
+// 	
+// 		document.body.appendChild(script);
+// 	
+// 		window.onSpotifyWebPlaybackSDKReady = () => {
+// 	
+// 			const playerConstructor = new window.Spotify.Player({
+// 				name: 'Web Playback SDK',
+// 				getOAuthToken: cb => { cb(spotifyUserAuthToken); },
+// 				volume: 0.5
+// 			});
+// 
+// 			player.current = playerConstructor;
+// 			// setPlayer(player);
+// 
+// 			player.current.addListener('ready', ({ device_id }) => {
+// 				console.log('Ready with Device ID', device_id);
+// 				deviceId.current = device_id;
+// 				// setDeviceId(device_id);
+// 			});
+// 	
+// 			player.current.addListener('not_ready', ({ device_id }) => {
+// 				console.log('Device ID has gone offline', device_id);
+// 			});
+// 	
+// 			player.current.addListener('player_state_changed', ( state => {
+// 
+//                 if (!state) {
+//                     return;
+//                 }
+// 
+//                 setCurrentTrack(state.track_window.current_track);
+//                 setIsPaused(state.paused);
+// 
+//                 player.current.getCurrentState().then( state => { 
+//                     (!state)? setIsPlaybackActive(false) : setIsPlaybackActive(true) 
+//                 });
+// 
+//             }));
+// 
+// 			player.current.on('playback_error', ({message}) => {
+// 				console.error('Failed to perform playback', message);
+// 			})
+// 
+// 			player.current.connect();
+// 	
+// 		};
+// 
+// 	}, []);
+// 
+// 	useEffect(() => {
+// 
+// 		async function transferPlayback() {
+// 
+// 			await fetch('https://api.spotify.com/v1/me/player', {
+// 				method: 'PUT',
+// 				headers: {
+// 					'Content-Type': 'application/json',
+// 					'Authorization': 'Bearer ' + spotifyUserAuthToken
+// 				},
+// 				body: JSON.stringify({
+// 					device_ids: [
+// 						deviceId.current
+// 					],
+// 					play: true
+// 				})
+// 			});
+// 
+// 		}
+// 
+// 		if (deviceId.current && activeCart) {
+// 			transferPlayback();
+// 		}
+// 
+// 	}, [deviceId, activeCart])
+// 
+// 	return (
+// 		<Fragment>
+// 			<div className='container'>
+// 				<div className='main-wrapper'>
+// 					{/*Empty 8-track player visual*/}
+// 					{/*Inside of that: activeCart details, if present*/}
+// 					{playerJSX}
+// 				</div>
+// 			</div>
+// 		</Fragment>
+// 	)
+// }
