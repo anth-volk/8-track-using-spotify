51a3ce0ec5a95d4bd4dd238e1de6c67ba1639925
Anthony Volk <anth.volk@gmail.com>
2023-05-27 01:25:42 -0500

    feat: Styling Home mostly complete

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -37,18 +37,28 @@ export default function Home() {
 			</div>
 			<div className="Home_about">
 				<div className="Home_about_halfSection">
-					<h2>What is App Name?</h2>
-					<p>Lorem Ipsum</p>
+					<h1>What is <span className="Util_logoInText">STEREO8s</span>?</h1>
+					<p>A web app that aims to pair <span className="Util_bold Util_italic">nostalgia</span> with <span className="Util_bold Util_italic">new technologies</span>. <span className="Util_logoInText">STEREO8s</span> allows users with a Spotify Premium&reg; account to create virtual 8-track "cartridges" that mimic the real 8-track experience, complete with songs sliced and distributed across the tape and hiss between tracks.</p>
 				</div>
 				<div className="Home_about_halfSection">
-					<img />
+					<img loading="lazy" />
 				</div>
 				<div className="Home_about_halfSection">
-					<h2>Why App Name?</h2>
-					<p>Lorem Ipsum</p>
+					<img loading="lazy" />
+				</div>
+				<div className="Home_about_halfSection">
+					<h1>Why <span className="Util_logoInText">STEREO8s</span>?</h1>
+					<p>Analog audio has seen a resurgence over the last decade, but much of the enthusiasm has been led by vinyl and cassettes. Other mediums, including 8-track tapes, have largely been left behind, even though they played a unique role in the history of analog audio. This web application is <a href="https://www.anthonyvolk.com">my</a> attempt to keep their legacy alive in the digital age.</p>
+				</div>
+				<div className="Home_about_halfSection">
+					<h1>What technology does <span className="Util_logoInText">STEREO8s</span> use?</h1>
+					<p><span className="Util_logoInText">STEREO8s</span> is a full-stack web applcation. The back end, which creates, stores, and deletes users' cartridges, is a RESTful API built using NodeJS and ExpressJS, connected to a PostgreSQL database via Sequelize ORM. The front end, including most requests to Spotify's API, is a ReactJS 17 application. For more information, check out the project's <a href="https://github.com/anth-volk/8-track-using-spotify">GitHub page</a>.</p>
+				</div>
+				<div className="Home_about_halfSection">
+					<img loading="lazy" />
 				</div>
 				<div className="Home_about_fullSection">
-					<h2>What features does App Name have?</h2>
+					<h1>What features does App Name have?</h1>
 				</div>
 				<div className="Home_about_fullSection">
 					<div className="Home_about_features">
