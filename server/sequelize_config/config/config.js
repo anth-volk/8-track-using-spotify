2f0a30c21d151f228412da3a330064d12fd868bb
Anthony Volk <anth.volk@gmail.com>
2023-06-08 23:50:41 -0500

    chore: Configure production database

--- server/sequelize_config/config/config.js
+++ server/sequelize_config/config/config.js
@@ -1,25 +1,26 @@
 require('dotenv').config();
 
 module.exports = {
-  "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME_DEV,
-    "host": process.env.DB_HOST,
-    "dialect": "postgres"
-  },
-  "test": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME_TEST,
-    "host": process.env.DB_HOST,
-    "dialect": "postgres"
-  },
-  "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME_PROD,
-    "host": process.env.DB_HOST,
-    "dialect": "postgres"
-  }
+	"development": {
+		"username": process.env.DB_USERNAME,
+		"password": process.env.DB_PASSWORD,
+		"database": process.env.DB_NAME_DEV,
+		"host": process.env.DB_HOST,
+		"dialect": "postgres"
+	},
+	"test": {
+		"username": process.env.DB_USERNAME,
+		"password": process.env.DB_PASSWORD,
+		"database": process.env.DB_NAME_TEST,
+		"host": process.env.DB_HOST,
+		"dialect": "postgres"
+	},
+	"production": {
+		"url": process.env.DB_PROD_URL,
+		"dialect": "postgres",
+		"ssl": true,
+		"dialectOptions": {
+			"ssl": true
+		}
+	}
 }
