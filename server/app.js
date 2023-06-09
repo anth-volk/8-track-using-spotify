f2fc52487629b1896f775da2324d06116f04f2c6
Anthony Volk <anth.volk@gmail.com>
2023-06-09 17:11:35 -0500

    test: Minimizing CORS settings

--- server/app.js
+++ server/app.js
@@ -47,10 +47,7 @@ else {
 }
 
 // Middleware
-app.use(cors({
-	credentials: true,
-	origin: [process.env.FRONTEND_TLD]
-}));
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/v1/protected', verifyJWT);
 app.use('/api/v1/spotify_api', spotifyAuthHeaders);
